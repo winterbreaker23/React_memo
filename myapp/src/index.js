f1e745d341fa2d84642921e6990c29ab5ff10136
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 20:56:45 +0100

    exp1

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { useState } from "react";
+import Todos from "./Todo";
 
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
